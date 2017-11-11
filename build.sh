ca5404695c7cfa879dc4fad3ef7fbca89ff6fe77
joezou <yixian.zou@gmail.com>
2017-11-11 22:07:41 +0800

    update build file.

--- build.sh
+++ build.sh
@@ -1,11 +1,42 @@
-read -p "enter your GOPATH: " goPath
-if [ -n "$goPath" ]; then
-    export GOPATH=$goPath
-    echo "GOPATH has change to $goPath"
-fi
+#!/bin/bash
 
-echo "installing seelog!"
+fileName="package.properties"
 
-gopm get github.com/cihub/seelog -v -g
+build(){
+    read -p "enter your GOPATH(no enter will use parent path of src): " goPath
+    echo "==============Set path=============="
+    if [ -n "$goPath" ]; then
+        export GOPATH=$goPath
+    else
+        currentPath=`pwd`
+        export GOPATH=${currentPath%src*}
+    fi
 
-echo "build successfully!"
+    echo "installing package!"
+
+    reimport
+
+    echo "build successfully!"
+}
+
+reimport() {
+    echo "==============import=============="
+    echo "Current GOPATH :"
+    echo $GOPATH
+
+    for line in `cat $fileName`
+    do
+        echo "installing $line!"
+        gopm get "$line" -v -g
+    done
+}
+
+case "$1" in
+  build)
+	build ;;
+  reimport)
+	reimport ;;
+  *)
+    echo $"Usage: $0 {build|reimport}"
+    exit 1
+esac
